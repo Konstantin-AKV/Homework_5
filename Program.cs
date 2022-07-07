dce3cea34833e368b791302e1538617f7051caf3
Konstantin-AKV <abdulaev-kostya@mail.ru>
2022-07-08 01:08:05 +0300

    Задача № 36

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 34: Массив и сколько в нем четных)
 
-void Print(int[] arr)
+/* void Print(int[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
@@ -8,14 +8,12 @@ void Print(int[] arr)
     }
     Console.WriteLine();
 }
-
 int[] mass = new int[10];
 for (int i = 0; i < mass.Length; i++)
 {
     mass[i] = new Random().Next(100, 1000);
 }
 Print(mass);
-
 int count = 0;
 for (int i = 0; i < mass.Length; i++)
     {
@@ -25,3 +23,33 @@ for (int i = 0; i < mass.Length; i++)
       }
     }
 Console.WriteLine("Количество четных чисел в массиве: " + count);
+ */
+
+ 
+
+// Задача 36: Задайте одномерный массив, заполненный
+// случайными числами. Найдите сумму элементов, стоящих
+// на нечётных позициях.
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+int[] mass = new int[4];
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        mass[i] = new Random().Next(-100, 100);
+    }
+}
+Print(mass);
+int sum = 0;
+for (int i = 1; i < mass.Length; i += 2)
+{
+    sum = sum + mass[i];
+}
+Console.WriteLine("Сумма чисел, стоящих в массиве на нечетной позиции: " + sum);
